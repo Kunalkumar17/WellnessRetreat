3ec846618bb1294613165c6559ddf5d25a13ba1b
Kunal kumar <97980205+Kunalkumar17@users.noreply.github.com>
2024-07-29 03:54:28 +0530

    Update RetreatCard.jsx

--- src/components/RetreatCard.jsx
+++ src/components/RetreatCard.jsx
@@ -1,7 +1,7 @@
 const RetreatCard = ({ image, title , description ,price ,date , location}) => {
   return (
-    <div className='flex h-[300px]  sm:w-[300px] sm:min-w-[380px] rounded-[20px] ease-in px-4 py-4 gap-4 cursor-pointer bg-green-50 hover:shadow-3xl'>
-        <div className="flex w-full h-full">
+    <div className='flex h-[300px]  sm:w-[300px] sm:min-w-[380px] rounded-[20px] pl-2 py-2 gap-4 cursor-pointer bg-green-50 hover:shadow-3xl'>
+        <div className="flex w-[400px]">
           <img src={image} alt={"image"} width={200} height={100}  className=" rounded-lg"/>
         </div>
         <div >
